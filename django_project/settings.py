1386e3bd10fb813c8814205c321be9df75ddecc9
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-17 14:17:28 +0500

    referencing templates

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = [,
+    'blog.app.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
