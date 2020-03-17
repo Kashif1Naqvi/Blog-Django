efd50a5ba4d3a31ef71063097d550d80fc8cd11b
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-17 15:01:21 +0500

    linking with templates

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-  return HttpResponse("<h1>Blog Home page :)</h1>")
+    return render(request,'blog/home.html')
 
 def about(request):
-  return HttpResponse("<h1>This is About page :) </h1>")
+    return render(request,'blog/about.html')
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -30,8 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [,
-    'blog.app.BlogConfig',
+INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
