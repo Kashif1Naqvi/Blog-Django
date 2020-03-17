f73b5aa67e789e3745bc597da5ba63b7b1a23f4b
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-17 17:12:45 +0500

    dummy data added

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author':'Kashif',
+        'title' :'Blog post 1',
+        'content':'Blog post content',
+        'date_posted':'March 17 2020'
+    },
+    {
+        'author':'Hassan',
+        'title' :'Blog post 2',
+        'content':'Blog post contents ',
+        'date_posted':'March 18 2020'
+    },
+    
+]
+
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
