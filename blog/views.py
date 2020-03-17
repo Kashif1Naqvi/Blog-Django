07afbf3c9e519a9644fa14d585575b520da0947d
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-17 17:40:59 +0500

    about page updated

--- blog/views.py
+++ blog/views.py
@@ -25,5 +25,5 @@ def home(request):
     return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title':'About'})
 
