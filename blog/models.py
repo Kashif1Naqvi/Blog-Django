4a1e94f55f59d377b958cbbe703b597f2fa22d8b
Kashif1Naqvi <40764353+Kashif1Naqvi@users.noreply.github.com>
2020-03-19 14:19:57 +0500

    model added with migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+"""
+  In this senario User with post make one to many relationship
+  one user do many posts,But post have only one user
+  for migrate the db after doing this typwing this command
+   python manage.py makemigrations
+   if check database how to work then type in cmd/terminal
+    python manage.py sqlmigrate blog 0001(any db reference who created in migration folder)
+"""
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author      = models.ForeignKey(User,on_delete=models.CASCADE)
 
-# Create your models here.
