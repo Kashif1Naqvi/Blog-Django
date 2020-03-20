b7ef169a28f5c7a926d6b6d3f181aa441da615b4
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-20 18:00:54 +0500

    adding email field

--- users/views.py
+++ users/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import UserRegistorForm
 def register(request):
     if request.method == 'POST':
-      form = UserCreationForm(request.POST)
+      form = UserRegistorForm(request.POST)
       if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        messages.success(request,f"Acount is created for {username}")
-        return redirect('blog-home')
+          form.save()
+          username = form.cleaned_data.get('username')
+          messages.success(request,f"Account is created for {username}")
+          return redirect('blog-home')
     else:
-        form = UserCreationForm()
-        
+          form = UserRegistorForm()
     return render(request,'users/register.html',{'form':form})
