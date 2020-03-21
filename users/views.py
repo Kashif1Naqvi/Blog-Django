96e535a7aaac525f7e80550787b124b0455adf68
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-22 00:52:22 +0500

    code updated

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistorForm
 def register(request):
     if request.method == 'POST':
@@ -12,3 +13,6 @@ def register(request):
     else:
           form = UserRegistorForm()
     return render(request,'users/register.html',{'form':form})
+@login_required
+def profile(request):
+      return render(request,'users/profile.html')
