95bbe56ca7079e5174e7267f50d53ebca7d30a71
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-21 14:17:15 +0500

    login logic completed

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from users import views as register_views
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/',register_views.register,name='register'),
+    path('register/',user_views.register,name='register'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='Login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='Logout'),
     path('', include('blog.urls') ),
 ]
