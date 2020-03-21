31bfe162f2500018a9bf866fd197b321ea9f3489
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-22 01:01:52 +0500

    clean data

--- blog/views.py
+++ blog/views.py
@@ -3,21 +3,6 @@ from django.http import HttpResponse
 from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author':'Kashif',
-        'title' :'''Programming''',
-        'content':'Now a days! Coding work is done very fast and solve our problem.so we try to avoid the all problem that we not fixed.so computer programming is basically telling the computer what to do',
-        'date_posted':'March 17 2020'
-    },
-    {
-        'author':'Haider',
-        'title' :'Prophet Muhammad',
-        'content':'Prophet Muhammad(saw) says! I am the city of knowledge Ali(as) is his Gate come for gate let me more! ',
-        'date_posted':'March 19 2020'
-    },
-]
-
 def home(request):
     context = {
         'posts':Post.objects.all()
