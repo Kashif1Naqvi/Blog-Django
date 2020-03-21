c71ed2a1263139f54745b398c094aeeb78e5ce66
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-22 00:35:30 +0500

    code updated

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,7 +20,7 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name='register'),
-    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='Login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='Logout'),
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('', include('blog.urls') ),
 ]
