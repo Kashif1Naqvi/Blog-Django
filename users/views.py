1a195a18cabaedf3c4b38b7019aea6c65528ed0a
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-25 22:43:12 +0500

    handle DoesNotExist profiles

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistorForm,UserUpdateForm,ProfileUpdateForm
+from .models import Profile
 def register(request):
     if request.method == 'POST':
       form = UserRegistorForm(request.POST)
@@ -13,19 +14,24 @@ def register(request):
     else:
           form = UserRegistorForm()
     return render(request,'users/register.html',{'form':form})
+		
 @login_required
 def profile(request):
+	try:
+		profile = request.user.profile
+	except Profile.DoesNotExist:
+		profile = Profile(user = request.user)
 	if request.method == 'POST':
 		u_form = UserUpdateForm(request.POST,instance = request.user)
-		p_form = ProfileUpdateForm(request.POST,request.FILES ,instance = request.user.profile)
-		if u_form.is_valid() & p_form.is_valid():
+		p_form = ProfileUpdateForm(request.POST,request.FILES ,instance = profile)
+		if u_form.is_valid() and p_form.is_valid():
 			u_form.save()
 			p_form.save()
 			messages.success(request,f"Your account has been updated!")
 			return redirect('profile')
 	else:
 		u_form = UserUpdateForm(instance=request.user)
-		p_form = ProfileUpdateForm(instance=request.user.profile)
+		p_form = ProfileUpdateForm(instance=profile)
 
 	context = {
 		'u_form' : u_form,
