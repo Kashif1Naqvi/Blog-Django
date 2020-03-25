372b268b82025cd454014bebc4812455b26bf37e
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-25 15:07:15 +0500

    users views updated

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistorForm
+from .forms import UserRegistorForm,UserUpdateForm,ProfileUpdateForm
 def register(request):
     if request.method == 'POST':
       form = UserRegistorForm(request.POST)
@@ -15,4 +15,10 @@ def register(request):
     return render(request,'users/register.html',{'form':form})
 @login_required
 def profile(request):
-      return render(request,'users/profile.html')
+      u_form = UserUpdateForm()
+      p_form = ProfileUpdateForm()
+      context = {
+            'u_form':u_form,
+            'p_form':p_form
+      }
+      return render(request,'users/profile.html',context)
