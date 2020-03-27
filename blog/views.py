a8f1dac291013d30a567b2543944a04fb0a10fbc
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-27 10:37:00 +0500

    user create post when login

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView,DetailView,CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 # Create your views here.
@@ -25,7 +26,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     fields = ['title','content']
 
