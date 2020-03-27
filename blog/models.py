478ad8ee36007ed0ee48e0b6d3700b6f260fb531
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-27 10:28:23 +0500

    create post logic added

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 """
   In this senario User with post make one to many relationship
   one user do many posts,But post have only one user
@@ -16,4 +17,7 @@ class Post(models.Model):
     author      = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
       return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post-detail",kwargs={'pk':self.pk})
     
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView
 from .models import Post
+
 # Create your views here.
 
 def home(request):
@@ -23,3 +24,12 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form): 
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+        
