e71acf72dcfbf34aa24187d1c8e1155b3a214c6f
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-27 11:35:58 +0500

    update post logic added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView,CreateView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
@@ -33,4 +33,10 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self,form): 
         form.instance.author = self.request.user
         return super().form_valid(form)
-        
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
