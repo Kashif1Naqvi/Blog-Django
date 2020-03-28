113fd8d963b0bbd9e6decc8d169f043819afc8db
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-28 08:50:24 +0500

    pagination added

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ class PostListView(ListView):
 
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
