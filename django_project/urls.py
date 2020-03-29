54d7c2a95190fd5bf57cd6ef2c9009e16459b2a0
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-29 12:03:57 +0500

    code updated

--- django_project/urls.py
+++ django_project/urls.py
@@ -25,6 +25,10 @@ urlpatterns = [
     path('register/',user_views.register,name='register'),
     path('profile/', user_views.profile , name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'
+      ),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('', include('blog.urls') ),
 ]
