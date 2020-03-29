8db1baba2d3b8935e21d00048d5b45b60b77d3c9
Kashif1Naqvi <mrkashif029@gmail.com>
2020-03-29 12:47:53 +0500

    password reset confirm done template created

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,7 +19,6 @@ from django.contrib.auth import views as auth_views
 from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_views.register,name='register'),
@@ -29,10 +28,14 @@ urlpatterns = [
          auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
          name='password_reset'
       ),
-    path('password-reset/done', 
+    path('password-reset/done/', 
         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
         name = 'password_reset_done'
-        ),
+    ),
+    path('password-reset-confirm/<uidb64>/<token>/', 
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        name = 'password_reset_confirm'
+    ),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('', include('blog.urls') ),
 ]
